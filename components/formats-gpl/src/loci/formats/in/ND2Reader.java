d1ecb59c0bbeddfbf28761c7e6f225d6c32fd47a
Matt <m.b.smith-5@umcutrecht.nl>
2025-09-26 10:53:50 +0200

    Use the width of the plane to calculate the scanline pad.

--- components/formats-gpl/src/loci/formats/in/ND2Reader.java
+++ components/formats-gpl/src/loci/formats/in/ND2Reader.java
@@ -342,7 +342,7 @@ public class ND2Reader extends SubResolutionFormatReader {
     }
     else {
       //The scanline pad is set at 4 byte boundaries described by the pixel size in bytes.
-      scanlinePad = ((bpp * w)%4) / bpp;
+      scanlinePad = ((bpp * getSizeX())%4) / bpp;
       // plane is not compressed
       readPlane(in, x, y, w, h, scanlinePad, buf);
     }
